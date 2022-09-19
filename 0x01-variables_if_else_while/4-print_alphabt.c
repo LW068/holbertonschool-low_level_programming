58ec2cd74520cb26c92f2a6a39885b5bdec5cc94
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 09:37:07 -0700

    sdfg

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,9 @@ int main(void)
 	char soup[24];"abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++);
 	{
-		putchar(soup[i])
+		putchar(soup[i]);
 	}
 	putchar('\n');
 	return (0);
-}

698e075b624a336fcc364933126b33d37d7ec6a0
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 09:14:59 -0700

    pushing p

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 #include<time.h>
 #include<stdio.h>
-
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
        char alpha[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";

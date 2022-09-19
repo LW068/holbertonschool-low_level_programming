c3f2bf24dac3d0648e7e0d192c551c6ab1b4eb1b
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 09:24:31 -0700

    fghjk

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char alpha[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	
-	for (i = 0; i < 52; i++
-		      {
-		      putchar(alpha[i]):
-		      }
-       putchar('\n');
-       return (0);
-}
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}	

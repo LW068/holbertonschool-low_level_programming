e903e278d328b4aae95d48264b2e532473ff8b28
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 11:11:29 -0700

    ghjk

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,13 +8,11 @@
 */
 int main(void)
 {
-	char alpha[26] = "zyxwvutsrqponmlkjihgfedcba";
-	int i;
+char alpha[26] = "zyxwvutsrqponmlkjihgfedcba";
+int i;
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alpha[i]);
-	}
-	putchar('\n');
-        return (0);
+for (i = 0; i < 26; i++)
+putchar(alpha[i]);
+putchar('\n');
+return (0);
 }

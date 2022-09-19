b324d130b930d273aca846120c40e1a7a82215f2
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 09:34:29 -0700

    dhjdh

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+/**
+* main - alphabet soup
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char soup[24];"abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(soup[i])
+	}
+	putchar('\n');
+	return (0);
+}

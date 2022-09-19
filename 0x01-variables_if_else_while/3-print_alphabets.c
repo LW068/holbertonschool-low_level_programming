1363f8a52e18e75bf97f6329309cfca82d139430
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 09:23:45 -0700

    sdfg

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
 */
 int main(void)
 {
-       char alpha[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-       int i;
-
-       for (i = 0; i < 52; i++)
-       {
-	       putchar(alpha[i]);
-       }
+	char alpha[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	
+	for (i = 0; i < 52; i++
+		      {
+		      putchar(alpha[i]):
+		      }
        putchar('\n');
        return (0);
 }

8093e16ba350fbae9a39eb63cad703ec0f9c2cf7
LW068 <ricardocoronaberron@gmail.com>
2022-09-19 19:40:47 -0700

    ghj

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,23 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 
 /**
-* main - TEXT
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int x;
+	int i;
 
-for (x = 48; x < 58; x++)
-{
-putchar(x);
-if (x != 57)
-}
-
-{
-putchar(',');
-putchar(' ');
-}
-putchar('\n');
-return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }

7aa03313dc43aacbe85900b799f61df776c78857
LW068 <ricardocoronaberron@gmail.com>
2022-09-22 18:32:22 -0700

    first try

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+  
+/**
+* PROTO - DESC
+*
+* Return: 0 if success.
+*/
+void print_numbers(void)
+{
+char num[10] = "0123456789";
+int a;
+{
+for (a = 0; a <= 10; a++)
+}
+_putchar(a[num]);
+_putchar("\n");
+{
+return (0);
+}
+}

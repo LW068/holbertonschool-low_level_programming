ca6ce8dc122bde8ed88df64022e9a050670096f1
LW068 <ricardocoronaberron@gmail.com>
2022-09-22 19:35:39 -0700

    hfhf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-* more_numbers - Print 0 to 14 10 times
+* more_numbers - Print 0 to 14 ten times.
 *
 * Return: No return.
 */
 void more_numbers(void);
 {
-	int a, b;
+int a, ch;
 
-	for (a = 0; a <= 10; a++)
-	{
-for (b = 0; b < 15; b++)
+for (a = 0; a <= 10; a++)
 {
-	if (b >= 10)
-		_putchar((b / 10) + 48);
-	_putchar((ch % 10) + 48);
+for (ch = 0; ch < 15; ch++)
+{
+if (ch >= 10)
+_putchar((ch / 10) + 48);
+_putchar((ch % 10) + 48);
 }
 _putchar('\n');
 }

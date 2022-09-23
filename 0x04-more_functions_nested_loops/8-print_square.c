15c8b2ed237b6207b31b258d70c3895ef37592d3
LW068 <ricardocoronaberron@gmail.com>
2022-09-23 08:08:22 -0700

    fhf

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,16 @@
 */
 void print_square(int size)
 {
-	int x, y;
+int x, y;
 
-	for (x = 0; x < size; x++)
-	{
-		for (y = 0; y < size; y++)
-			_putchar(35);
-	}
-	_putchar('\n');
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+if (x != size - 1)
+_putchar('\n');
+}
+_putchar('\n');
 }

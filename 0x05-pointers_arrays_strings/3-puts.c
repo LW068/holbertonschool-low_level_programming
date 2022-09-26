db24134971a4391eaf440cd1dc832ae4eda773bb
LW068 <ricardocoronaberron@gmail.com>
2022-09-26 09:56:19 -0700

    strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* _puts - function that prints a string, followed by a new line, to stdout.
-*
-* @str: string
-*
-* Return: no return.
-*/
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
 void _puts(char *str)
 {
 	int count = 0;

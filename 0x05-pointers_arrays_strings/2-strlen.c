88233c0e75125ee407b249d9861477844a683df3
LW068 <ricardocoronaberron@gmail.com>
2022-09-26 09:27:36 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,16 @@
 
 /**
  * _strlen - returns the length of a string.
- * 
+ *
  * @s: input string.
- * 
+ *
  * Return: length of a string.
  */
 int _strlen(char *s)
 {
-	int count = 0;
+int count = 0;
 
-	while (*(s + count) != '\0')
-		count++;
-	return (count);
+while (*(s + count) != '\0')
+count++;
+return (count);
 }

97da24609a84d5f6e71cf10002f78ffb8a4ba8f3
LW068 <ricardocoronaberron@gmail.com>
2022-09-29 14:32:42 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* *_strncpy - function taht copies a string.
+*
+* @dest: a string
+* @src: a string.
+* @n: character(s).
+*
+* Return: 0 is success.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (0);
+}

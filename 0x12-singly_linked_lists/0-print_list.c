3d76aa2d5a7cb4cdf40852df69e0aa958dd8dacf
LW068 <ricardocoronaberron@gmail.com>
2022-11-07 09:56:33 -0800

    function that prints all the elements of a listv1.1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t lmnts;
+	size_t nelem;
 
-	lmnts = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		lmnts++;
+		nelem++;
 	}
-	return (lmnts);
+	return (nelem);
 }

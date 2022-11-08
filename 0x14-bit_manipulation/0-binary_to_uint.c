9fca10d08885e03bcafee94ab7cc79d110cdca1f
LW068 <ricardocoronaberron@gmail.com>
2022-11-08 15:12:04 -0800

    fixed added library

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
@@ -11,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int x = 0;
 	int y = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 	while (b[y])
 	{

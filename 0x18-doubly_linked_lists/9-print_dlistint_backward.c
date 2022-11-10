22dfdcc9498f057386fd9cb4099891f6595d03f3
LW068 <ricardocoronaberron@gmail.com>
2022-11-10 15:19:28 -0800

    prints all the elements of a dlistint_t list backward

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *print_dlistint_backward - print list backwards.
+ *
+ * @h: Head of list
+ *
+ * Return: Count of Elements
+ */
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	size_t c;
+
+	if (h == NULL)
+		return (0);
+	while (h->next)
+		h = h->next;
+	for (c = 0; h; c++, h = h->prev)
+		printf("%i\n", h->n);
+	return (c);
+}
